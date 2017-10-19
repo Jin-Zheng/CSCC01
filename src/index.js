125ff7c6519bd7f4cc237aaa6d4f4b1d29b1c6fe
xxcandiez <josephdong1998@gmail.com>
2017-10-19 16:17:30 -0400

    added FormatListView component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FormatListView from './components/formatListView.js'
+import {List} from 'immutable'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Index extends React.Component {
+  render() {
+    const dummyList = List(['hi', 'my', 'name', 'is'])
+
+    return (
+      <div>
+        <FormatListView list={dummyList}/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'))
