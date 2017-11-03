0eb902b716092911b23a7ff9e2d18fc8b5cba7a3
xxcandiez <josephdong1998@gmail.com>
2017-11-02 20:38:51 -0400

    more concise apps

--- react_frontend/src/apps/generateApp.js
+++ react_frontend/src/apps/generateApp.js
@@ -1,8 +1,38 @@
 import React from 'react'
 import {Row, Col} from 'react-flexbox-grid'
-import GenerateSwitcher from '../switchers/generateSwitcher'
-import GenerateRouter from '../routers/generateRouter'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom'
+import CreateShortAnswer from '../components/question/createShortAnswer'
+import CreateMultipleChoice from '../components/question/createMultipleChoice'
+
+class GenerateRouter extends React.Component {
+  render() {
+    return(
+      <Row>
+          <Link to='/generateApp/createShortAnswer'>
+            short answer|
+          </Link>
+          <Link to='/generateApp/createMultipleChoice'>
+            multiple choice
+          </Link>
+      </Row>
+    )
+  }
+}
+
+class GenerateSwitcher extends React.Component {
+  render() {
+    return(
+      <Switch>
+        <Route
+          exact path='/generateApp/createShortAnswer'
+          component={CreateShortAnswer}/>
+        <Route
+          exact path='/generateApp/createMultipleChoice'
+          component={CreateMultipleChoice}/>
+      </Switch>
+    )
+  }
+}
 
 class GenerateApp extends React.Component {
   render() {
