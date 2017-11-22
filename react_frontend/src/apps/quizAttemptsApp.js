81b5c70c6151c04c36c31ee33a16877e9da9cc64
xxcandiez <josephdong1998@gmail.com>
2017-11-21 21:58:45 -0500

    added 3 classes to quizteempt.js

---
+++ react_frontend/src/apps/quizAttemptsApp.js
@@ -0,0 +1,13 @@
+import React form 'react'
+
+class quizAttemptRouter extends React.Component {
+
+}
+
+class quizAttemptSwticher extends React.Component {
+
+}
+
+class quizAttemptApp extends React.Component {
+  
+}
